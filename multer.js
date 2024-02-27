ae20ef2c76dfe396a87c04cf4743a5c448a0b97b
Arbaj_1147 <shaikharbaj2001@gmail.com>
2024-02-28 00:00:38 +0530

    update multer for vercel

--- multer.js
+++ multer.js
@@ -1,9 +1,9 @@
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
+  // destination: function (req, file, cb) {
+  //   cb(null, "uploads/");
+  // },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     const filename = file.originalname.split(".")[0];
@@ -11,6 +11,9 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage,limits:{fieldSize: 25 * 1024 * 1024}});
+const upload = multer({
+  storage: storage,
+  limits: { fieldSize: 25 * 1024 * 1024 },
+});
 
 module.exports = upload;
